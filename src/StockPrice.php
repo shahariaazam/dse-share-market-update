a7cb5a4cff1ac5c6f8eec5621016a4c1d90038a2
Shaharia Azam <shaharia.azam@gmail.com>
2019-07-22 03:10:50 +0600

    Check whether cURL has been enabled on server. Due to PHP7.2 compatibility file_get_contents() has been replaced with cURL request

--- src/StockPrice.php
+++ src/StockPrice.php
@@ -9,10 +9,14 @@ class StockPrice
 {
     /**
      * StockPrice constructor.
+     * @throws \Exception
      */
     public function __construct()
     {
-
+        //Check whether cURL is enabled
+        if(!function_exists('curl_version')){
+            throw new \Exception("cURL is not enabled in your server.");
+        }
     }
 
     /**
